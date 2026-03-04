7e92358ec5f1552f1efc380bec8610da83e9790b
DanielPimbor <pimbor.daniel@diak.szbi-pg.hu>
2026-03-04 09:52:33 +0100

    uzemanyag es fogyasztas

--- auto.py
+++ auto.py
@@ -1,9 +1,11 @@
 class Auto:
-    def __init__(self, marka_, tipus_, gyartasi_ev):
+    def __init__(self, marka_, tipus_, gyartasi_ev, fogyasztas_ = 7.5, uzemanyag_=50):
         self.marka = marka_
         self.tipus = tipus_
         self.gyartasi_ev = gyartasi_ev
         self.sebesseg = 0
+        self.fogyasztas = fogyasztas_
+        self.uzemanyag= uzemanyag_
     
     def __str__(self):
         return f'{self.marka} {self.tipus} ({self.gyartasi_ev}) sebesség: {self.sebesseg} km/h'
@@ -12,8 +14,18 @@ class Auto:
         self.sebesseg += ertek
         if self.sebesseg > 200:
             self.sebesseg = 200
+            print('200-al mész.')
 
     def fekezz(self, ertek):
         self.sebesseg -= ertek
         if self.sebesseg < 0:
             self.sebesseg = 0
+            print('Megálltál')
+
+#mindegy hogy erteek vagy mennyiseg azzal csak megnevezzuk
+
+    def tankol(self, mennyiseg):
+        self.uzemanyag += mennyiseg
+        if self.uzemanyag > 50:
+            self.uzemanyag = 50
+            print('Tele a tank.')
