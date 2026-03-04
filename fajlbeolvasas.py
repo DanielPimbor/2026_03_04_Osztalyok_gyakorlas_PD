5df59d116841fbc989caaad2648a63c43648dd16
DanielPimbor <pimbor.daniel@diak.szbi-pg.hu>
2026-03-04 11:15:03 +0100

    auto txt

---
+++ fajlbeolvasas.py
@@ -0,0 +1,16 @@
+from auto import Auto
+
+autok = []
+with open('adatok/autok.txt', 'r', encoding='utf-8') as forrasfajl:
+    next(forrasfajl)
+    for sor in forrasfajl:
+        adatok = sor.strip().split(',')
+        if len(adatok) >= 4:
+            marka, tipus, gyartasi_ev, fogyasztas = adatok
+        auto = Auto(marka, tipus, gyartasi_ev, fogyasztas)
+        autok.append(auto)
+
+# print(f'{autok=}')
+
+for auto in autok:
+    print(auto)
